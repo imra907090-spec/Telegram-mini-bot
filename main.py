6391f3a7c1bd97892ffcd8f9787096456bf67c47
imra907090-spec <imra907090@gmail.com>
2026-07-08 20:32:19 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 import json
 import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
